c80b3633ea77313fabfd3f5f9275154e7f2109fa
Williamvilla25 <williamvilla727@gmail.com>
2025-05-28 01:00:52 -0500

    validacion de roles

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,7 +19,8 @@
                 </div>
             @endif
 
-            @if(Auth::user()->role === 'admin')
+            
+            @if(in_array(Auth::user()->role, ['admin', 'cliente']))
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('users') }}

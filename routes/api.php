c4ecd098bb6da28f8fe266658f9946b87c1b92e4
Janeer Barrera <barrera.janeer@gmail.com>
2025-05-29 00:09:27 -0500

    Api Suppliers

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BranchApiController;
-use App\Http\Controllers\api\PizzaApiRawMaterial;
+use App\Http\Controllers\Api\PizzaApiRawMaterial;
 use App\Http\Controllers\Api\PurchasesApiController;
 use App\Http\Controllers\Api\RawApiController;
-
-
-
+use App\Http\Controllers\Api\SuppliersApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,19 +21,21 @@ use App\Http\Controllers\Api\RawApiController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-
-    
 });
-//Branches Routes
+
+// Branches Routes
 Route::apiResource('branches', BranchApiController::class);
 
-//PizzaRawMaterial Routes
+// PizzaRawMaterial Routes
 Route::get('/pizza-raw-materials', [PizzaApiRawMaterial::class, 'index']);
 
-//Purchases Routes
+// Purchases Routes
 Route::apiResource('purchases', PurchasesApiController::class);
 
-//Raw-materials 
+// Suppliers Routes
+Route::apiResource('suppliers', SuppliersApiController::class, );
+
+// Raw-materials Routes
 Route::get('/raw-materials', [RawApiController::class, 'index']);
 Route::get('/raw-materials/{id}', [RawApiController::class, 'show']);
 Route::post('/raw-materials', [RawApiController::class, 'store']);
